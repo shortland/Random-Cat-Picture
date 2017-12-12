84424536b1f747c4c512e242f15c2ec38093e80f
Ilan Kleiman <ilankleiman@gmail.com>
2017-12-12 15:30:02 -0500

    faster & better

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Random-Cat-Picture
-Redirects you to a random cat picture; being used for a Discord Bot
+Loads a random cat meme-y pic eachtime you load the page.
 
-Working Example: http://ilankleiman.com/katmeme.pl (will redirect to a random cat pic hosted on http://www.lolcats.com)
+Currently being used for a Discord Bot
+
+Working Example: http://ilankleiman.com/projects/KatMeme/katmeme.pl
+
+Scrapes pictures from  http://www.lolcats.com
+
+##### Disclaimer: I wrote this script just for fun. In no way affiliated with 'http://www.lolcats.com' Use at your own risk.
--- katmeme.pl
+++ katmeme.pl
@@ -1,43 +1,61 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
 use CGI;
+use CGI::Carp qw(fatalsToBrowser);
 
-BEGIN
-{
-    $cgi = new CGI;
+sub random_kitty {
+	my ($try) = @_;
+	my $cgi = new CGI;
+	my $base_url = "http://www.lolcats.com";
+	#Max tries.
+	if ($try eq 10) {
+		print $cgi->header(-type => "text/plain");
+		return "Error getting that picture :( Try refreshing the page.";
+	}
+	#random page
+	#There ARE this many pages. But like ~5% of them return "BAD GATEWAY" for whatever reason?...
+	#So we need to check to see if it returns bad GATEWAY then get a new page if it does...
+	my $r_page = (int(rand(137)) + 2);
+	my $p_data = request_curl("${base_url}/gallery/popular-${r_page}.html");
+	if (is502($p_data)) {
+		return random_kitty(++$try);
+	}
+	my @image_urls = ($p_data =~ m/<td class="gallery-item">\s+<a href="\/popular\/\d+\.html">\s+<img src=(\S+)/g);
+	#48 random cats per page
+	#remove 0-47 cats, to get a random cat
+	my $random_pic = int(rand(48));
+	my $url_use = $image_urls[$random_pic] =~ s/\/thumbs|"//gr;#/
+	if (!defined $url_use || $url_use eq "") {
+		return random_kitt(++$try);
+	}
+	my $img_data = request_curl("${base_url}${url_use}");
+	if (is502($img_data)) {
+		return random_kitty(++$try);
+	}
+	#mostly unecessary, but decent for debugging ¯\_(ツ)_/¯
+	print $cgi->header(-type => "image", -cookie => ($cgi->cookie(-name => 'tries', -value => $try, -expires => '+4h', -path => '/')));
+	#print $cgi->header();
+	return $img_data;
 }
-STARTER:
-# random page
-$randomPage = (int(rand(137)) + 2);
 
-$pageData = `curl -s -A "This is a script, please let us use your site to find random pics <3 ty." "http://www.lolcats.com/gallery/popular-$randomPage.html" -L`;
-
-# 48 random cats per page
-# remove 0-47 cats, to get a random cat
-$randomPic = int(rand(48));
-
-$pageData =~ s/gallery-item/Ω/g;
-$pageData =~ s/<\/td>/∑/g;
-
-for(0..$randomPic)
-{
-    ($rmThis) = ($pageData =~ /[^Ω]*Ω([^∑]+)/);
-    $pageData =~ s/$rmThis//g;
+sub request_curl {
+	my ($url) = @_;
+	my $data = `curl -s -A "This is a script, ilankleiman\@gmail.com" "$url" -L`;
+	return $data;
 }
 
-# use this
-($partitioned) = ($pageData =~ /[^Ω]*Ω([^∑]+)/);
-($useThis) = ($partitioned =~ /src=[^"]*"([^"]+)/);
-
-$useThis =~ s/thumbs\///g;
-if(($useThis =~ "") || (!defined $useThis))
-{
-    goto STARTER;
+sub is502 {
+	my ($p_data) = @_;
+	if ($p_data =~ m/<h\w{3}><t\w{4}>502/g) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
 
-print $cgi->header(-type => "image");
-
-# directly print image rather than redirect
-print `curl -s "http://www.lolcats.com/$useThis"`;
-
-open(STDERR, ">&STDOUT");
+BEGIN {
+    print random_kitty(0);
+}
